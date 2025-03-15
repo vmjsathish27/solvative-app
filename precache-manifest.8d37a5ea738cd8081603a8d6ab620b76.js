3e75ac70fa9c04d481b17732eb818e23d918a5ad
vmjsathish27 <vmjsathish27@gmail.com>
2025-03-15 21:13:10 +0530

    Updates

---
+++ precache-manifest.8d37a5ea738cd8081603a8d6ab620b76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb3f51d97cb153ac3e6bbf76456a3f5",
+    "url": "/solvative-app/index.html"
+  },
+  {
+    "revision": "5df5301526a8500d3170",
+    "url": "/solvative-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "3556b739f612f65aceef",
+    "url": "/solvative-app/static/js/2.53ff63a9.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/solvative-app/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "5df5301526a8500d3170",
+    "url": "/solvative-app/static/js/main.6d9fbb3e.chunk.js"
+  },
+  {
+    "revision": "327a6730cd753f03c3a3",
+    "url": "/solvative-app/static/js/runtime~main.32c3d2e4.js"
+  }
+]);
